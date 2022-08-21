1836e793d11bdfebeb92aff490804bc366bb9513
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-08-21 14:43:17 +0700

    Introducing to Module

---
+++ 01. Introducing to Module/main.mjs
@@ -0,0 +1,6 @@
+// to use module in other file we need import it
+// module use extentions .mjs but we can use .js
+import { sayHello, sayGoodbye } from "./module.mjs";
+
+sayHello("Daffa Haidar"); // we can use this function in other file
+sayGoodbye("Daffa Haidar"); // we can use this function in other file
---
+++ 01. Introducing to Module/module.mjs
@@ -0,0 +1,9 @@
+// to be able to use this function in other files we need export it
+
+export function sayHello(name) {
+  console.log("Hello " + name);
+}
+
+export function sayGoodbye(name) {
+  console.log("Goodbye " + name);
+}
