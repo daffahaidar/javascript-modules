56daa311cf85b64ae8a26e7b3e5be57b6f95bb3c
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-10-04 11:14:37 +0700

    Belajar Javascript Module By Eko Kurniawan Khannedy

--- 01. Introducing to Module/module.mjs
+++
@@ -1,9 +0,0 @@
-// to be able to use this function in other files we need export it
-
-export function sayHello(name) {
-  console.log("Hello " + name);
-}
-
-export function sayGoodbye(name) {
-  console.log("Goodbye " + name);
-}
--- 02. Class in Module/main.mjs
+++
@@ -1,4 +0,0 @@
-import { Person } from "./class.mjs";
-
-const person = new Person("Daffa", 21);
-person.sayHello();
